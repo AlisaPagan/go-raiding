588fcb3e5ab7848a9ee1ea04181465add52ae0ee
Your Name <alisapodgaynaya023@gmail.com>
2026-03-07 20:15:03 -0700

    create api files, i axios, basic api and raids api

---
+++ apps/web/src/services/raids.api.ts
@@ -0,0 +1,17 @@
+import api from "./api";
+export type Raid = {
+  id: string;
+};
+export type GetRaidsParams = Record<string, string | number | boolean | undefined>;
+
+export type GetRaidsResponse = Raid[];
+
+export async function getRaids(params: GetRaidsParams): Promise<GetRaidsResponse> {
+  const { data } = await api.get<GetRaidsResponse>("/raids", { params });
+  return data;
+}
+
+export async function getRaidById(id: string): Promise<Raid> {
+  const { data } = await api.get<Raid>(`/raids/${id}`);
+  return data;
+}
