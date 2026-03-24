1965d5f69e1610484ecc574a2e6f9b6bc3ca0309
Your Name <alisapodgaynaya023@gmail.com>
2026-03-23 21:54:25 -0600

    FEAT: make custom select logic UI: style partially select & options

--- apps/web/src/components/UI/Select/Select.tsx
+++ apps/web/src/components/UI/Select/Select.tsx
@@ -1,5 +1,7 @@
+import Icon from "../Icon/Icon";
 import styles from "./Select.module.css";
-import type { SelectProps } from "./SelectPropsTypes";
+import type { SelectOption, SelectProps } from "./SelectPropsTypes";
+import { useState } from "react";
 
 function Select({
   id,
@@ -11,6 +13,68 @@ function Select({
   onChange,
   disabled,
   error,
-}: SelectProps) {}
+}: SelectProps) {
+  //select closed
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  //find an option in the list
+  const selectedOption = options.find((option) => {
+    return option.value === value;
+  });
+
+  //open/close select
+  function handleToggle() {
+    if (disabled) {
+      return;
+    }
+    setIsOpen((prev) => !prev);
+  }
+
+  //select an option
+  function handleOptionSelect(selectedOption: SelectOption) {
+    if (selectedOption.disabled) {
+      return;
+    }
+    onChange(selectedOption.value);
+    setIsOpen(false);
+  }
+
+  return (
+    <div className={styles.selectWrapper}>
+      {label && (
+        <label htmlFor={id} className={styles.label}>
+          {label}
+        </label>
+      )}
+      <button
+        type="button"
+        className={styles.selectControl}
+        onClick={handleToggle}
+        disabled={disabled}
+        id={id}
+        name={name}
+      >
+        <span>{selectedOption?.label || placeholder}</span>
+        <Icon name="icon-chevron-vert" size={15} className={styles.dropdownIcon} />
+      </button>
+      {isOpen && (
+        <div className={styles.optionsList}>
+          {options.map((option) => (
+            <button
+              key={option.value}
+              onClick={() => handleOptionSelect(option)}
+              type="button"
+              disabled={option.disabled}
+              className={styles.option}
+            >
+              {option.label}
+            </button>
+          ))}
+        </div>
+      )}
+      {error && <p className={styles.errorText}>{error}</p>}
+    </div>
+  );
+}
 
 export default Select;
